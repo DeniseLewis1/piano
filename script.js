376ca2560a5a0b1c2e11cb645350a972de72b6c7
Denise <deniselewis1120@gmail.com>
2023-06-08 17:21:32 -0700

    Updated keys to play correct sound

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 const pianoKeys = document.querySelectorAll(".key");
+const keysMap = ["02", "04", "06", "09", "11", "14", "16", "18", "21", "23", "01", "03", "05", "07", "08", "10", "12", "13", "15", "17", "19", "20", "22", "24"]
 
 function playSound(newUrl) {
     new Audio(newUrl).play();
     console.log(newUrl);
 }
 
-
 pianoKeys.forEach((pianoKey, i) => {
-    const number = i < 9 ? "0" + (i + 1) : (i + 1);
-    const newUrl = `24-piano-keys/key${number}.mp3`;
+    const newUrl = `24-piano-keys/key${keysMap[i]}.mp3`;
     pianoKey.addEventListener("click", () => playSound(newUrl));
 })
